7ccfd5552aef7753869ccad9c74b177df0534929
second-profile <nedel439@gmail.com>
2025-04-18 11:55:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,21 @@ function toggleMode() {
 
 // إضافة حدث عند الضغط على الزر
 document.querySelector(".toggle-theme").addEventListener("click", toggleMode);
+
+
+function updateThemeColor() {
+    const metaThemeColor = document.querySelector("meta[name=theme-color]");
+    const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    if (isDarkMode) {
+      metaThemeColor.setAttribute("content", "#333"); // لون الوضع الداكن
+    } else {
+      metaThemeColor.setAttribute("content", "##89CFF0"); // لون الوضع الفاتح
+    }
+  }
+
+  // استدعاء الدالة عند تحميل الصفحة
+  updateThemeColor();
+
+  // مراقبة تغيّر الوضع تلقائيًا
+  window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", updateThemeColor);
